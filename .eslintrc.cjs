9a9c632dd4462bf0912abd8d6422ae672461b940
Gustavo Silva <gugassilva2000@gmail.com>
2024-06-20 19:38:00 -0300

    configuring eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,7 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
+    '@rocketseat/eslint-config/react',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
@@ -14,5 +15,8 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+  },
+  parserOptions: {
+    warnOnUnsupportedTypeScriptVersion: false,
   },
 }
