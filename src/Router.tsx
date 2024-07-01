3412e0a61ac5ce866527c9e49950a4b50e1b84da
Gustavo Silva <gugassilva2000@gmail.com>
2024-07-01 18:45:19 -0300

    testing routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,9 +6,9 @@ import { DefaultLayout } from './layouts/DefaultLayout'
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<DefaultLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/history" element={<History />} />
+      <Route path="/ignite-timer/" element={<DefaultLayout />}>
+        <Route path="/ignite-timer/" element={<Home />} />
+        <Route path="/ignite-timer/history" element={<History />} />
       </Route>
     </Routes>
   )
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,10 +8,10 @@ export function Header() {
     <HeaderContainer>
       <img src={logoIgnite} alt="" />
       <nav>
-        <NavLink to="/" title="Timer">
+        <NavLink to="/ignite-timer/" title="Timer">
           <Timer size={24} />
         </NavLink>
-        <NavLink to="/history" title="Histórico">
+        <NavLink to="/ignite-timer/history" title="Histórico">
           <Scroll size={24} />
         </NavLink>
       </nav>
